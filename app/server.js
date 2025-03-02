e24f8a9c2f989e16a4f6a0bae16327241442fcb0
NISHANTH V S <nishanthvs0320@gmail.com>
2025-03-02 12:09:51 +0530

    Initial commit

--- app/server.js
+++ app/server.js
@@ -178,7 +178,6 @@ app.use(function (req, res) {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    const PORT = process.env.PORT || 3000;
+    console.log(`Server running on port ${PORT}`);
 });
