5693c080ed9cb6580cf4110a2b5d4a446085f39b
NISHANTH V S <nishanthvs0320@gmail.com>
2025-03-02 09:54:51 +0530

    Initial commit

--- app/server.js
+++ app/server.js
@@ -89,7 +89,7 @@ require("dotenv").config();  // Load environment variables
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
